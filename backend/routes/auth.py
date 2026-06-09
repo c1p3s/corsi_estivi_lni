7aab1f90946507d70783762a2acb1c3c1cacfb6b
Vincenzo Pagano <vincenzo.pagano.12008@gmail.com>
2026-06-09 15:35:32 +0200

    torno a prima

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, redirect, url_for, render_template, session
-from backend.json_reader import config_password
+from json_reader import config_password
 
 auth_bp = Blueprint("auth_bp", __name__)
         
